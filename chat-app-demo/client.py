b0086a6630dc2a739f2bea15afd7a8a0ad42ee1f
Siphesihle Mhlongo <sihlecodes@gmail.com>
2023-07-25 22:55:35 +0200

    Can now display messages from server

--- chat-app-demo/client.py
+++ chat-app-demo/client.py
@@ -5,6 +5,8 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.properties import ListProperty
+from kivy.event import EventDispatcher
+from kivy.clock import Clock
 
 import socket
 import constants
@@ -17,47 +19,48 @@ class CustomLabel(Label):
     pass
 
 class MessageHandler(threading.Thread):
-    def __init__(self, connection, messages, *args, **kwargs):
+    def __init__(self, connection, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.connection = connection
-        self.messages = messages
 
     def run(self):
         while True:
             try:
                 message = self.connection.recv(1024).decode(constants.FORMAT)
 
-                if message:
-                    self.messages.append(message)
-            finally:
+                if message and hasattr(self, "callback"):
+                    self.callback(message)
+
+            except Exception as e:
+                print("Broken connection: ", self.connection)
                 break
-            # except Exception as e:
-            #     raise e
-            #     print("Broken connection: ", self.connection)
-            #     break
 
 class ClientUI(BoxLayout):
-    messages = ListProperty([])
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # self.connection = connection
 
         layout = self.ids.message_container
         layout.bind(minimum_height = layout.setter("height"))
 
-    def on_messages(self, instance, messages):
-        label = CustomLabel(text=messages[-1])
+    def _on_new_message(self, message):
+        Clock.schedule_once(lambda x: self.add_message(message))
+    
+    def add_message(self, message):
+        label = CustomLabel(text=message)
         self.ids.message_container.add_widget(label)
 
 class ClientApp(App):
     def __init__(self, connection, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.ui = ClientUI()
-        self.message_handler = MessageHandler(connection, self.ui.messages)
+        self.message_handler = MessageHandler(connection)
 
     def build(self):
-        return self.ui
+        ui = ClientUI()
+
+        self.message_handler.callback = ui._on_new_message
+        self.message_handler.start()
+
+        return ui 
 
 if __name__ == '__main__':
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
