548cfd7f33bf6c826d34948634eba86a17ddbc0d
Siphesihle Mhlongo <sihlecodes@gmail.com>
2023-07-27 21:56:52 +0200

    Added guard clause

--- chat-app-demo/utils.py
+++ chat-app-demo/utils.py
@@ -4,7 +4,11 @@ def encode(message: str) -> bytes:
     return bytes(f"{len(message):<{constants.HEADER_LENGTH}}{message}", constants.FORMAT)
 
 def get_message(connection) -> str:
-    length: int = int(connection.recv(constants.HEADER_LENGTH))
-    message: str = connection.recv(length).decode(constants.FORMAT)
+    header: bytes = connection.recv(constants.HEADER_LENGTH)
+    message: str = ""
+
+    if header:
+        length: int = int(header)
+        message = connection.recv(length).decode(constants.FORMAT)
 
     return message
