1466a88b1a6d71636dc34eb3732fbd5c8d1db2c0
Siphesihle Mhlongo <sihlecodes@gmail.com>
2023-09-13 21:13:26 +0200

    Updated server functionality example cases

--- simple-event-sockets/server.py
+++ simple-event-sockets/server.py
@@ -2,11 +2,11 @@ from ses import ESServer
 
 server = ESServer("localhost", 9898)
 
-def _on_client_connection(client, *args, **kwargs):
-    client.emit("message", "Hello, world!")
+def _on_client_connection(client):
+    server.emit_on(client, "message", "Hello, world!")
 
-def _on_client_disconnect(client, *args, **kwargs):
-    client.emit("message", "Goodbye, world!")
+def _on_client_disconnect(client):
+    server.emit_on(client, "message", "Goodbye, world!")
 
 server.on("connection", _on_client_connection) # emitted internally
 server.on("disconnect", _on_client_disconnect) # emitted internally
